fc07347b544161fe100e81c3054a3d8b9a851519
Dello <vicctheerapper@gmail.com>
2024-01-12 23:28:13 +0100

    hurah

---
+++ app/search/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/Header";
+import MainCarousel from "@/app/components/MainCarousel";
+import "@/app/dist/style/App.css";
+
+function Search() {
+  return (
+    <>
+      <Header page="search" />
+    </>
+  );
+}
+
+export default Search;
