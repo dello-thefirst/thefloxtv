1cc2974301fa708ea9856a76102903d5010e5a28
Dello <vicctheerapper@gmail.com>
2024-02-14 12:54:35 +0100

    tregbe

--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -75,7 +75,11 @@ export default function Trending(props: TrendingProp) {
                   className="item w-[150px] h-[220px] mr-[10px] overflow-hidden relative sm:w-[120px] sm:h-[190px] sm:mr-[8px]"
                   style={{ flex: "0 0 auto" }}
                 >
-                  <div className="mask w-full h-full bg-opacity-20 bg-black absolute"></div>
+                  <div className="mask w-full h-full bg-opacity-60 bg-black absolute opacity-0 hover:opacity-100 transition-[0.3s] center-div">
+                    <div className="play w-[20px] h-[20px] rounded-full bg-[#ff2d7e] text-black center-div">
+                      <i className="fa-solid fa-play text-[10px]"></i>
+                    </div>
+                  </div>
 
                   <Image
                     className="object-cover w-full h-full"
