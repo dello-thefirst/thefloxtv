e03cd0015f79e932c83ce3c50230b1c02085b8a2
Dello <vicctheerapper@gmail.com>
2024-02-20 21:45:20 +0100

    jdos;lui

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -22,11 +22,11 @@ console.log("Current day: " + currentDay);
 
 function Calendar() {
   return (
-    <div className="w-[33px] h-[33px] bg-white rounded-md overflow-hidden inline-block flex-col text-[8px] text-black">
-      <div className="top w-full h-[12px] bg-red-600 text-center center-div text-white">
+    <div className="fh w-7 h-7 bg-white rounded-md overflow-hidden inline-block flex-col text-[8px] text-black sm:w-6 sm:h-6" style={{textShadow: "none"}}>
+      <div className="top w-full h-[10px] bg-red-600 text-center center-div text-white">
         {currentMonthAbbreviation}
       </div>
-      <div className="day w-full h-[17px] text-center center-div text-[15px] font-sans font-semibold">
+      <div className="day w-full h-[17px] text-center center-div text-[12px] font-sans font-semibold">
         {currentDay}
       </div>
     </div>
