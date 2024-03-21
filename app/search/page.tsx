6ec6b904530b7c491d457be6301539ecad45bfba
Dello <vicctheerapper@gmail.com>
2024-03-21 14:36:24 +0100

    gkcggh

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Header from "@/app/components/Header";
-import "@/app/dist/style/App.css";
 import Recent from "../components/Recent";
 
 function Search() {
