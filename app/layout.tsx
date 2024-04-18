50ae97994462567c4d23cb6407cab0ee2ff7a15d
Dello <vicctheerapper@gmail.com>
2024-04-18 14:10:22 +0100

    sjbwjxbwbs

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,12 +49,12 @@ export default function RootLayout({
             color="red"
             initialPosition={0.08}
             crawlSpeed={200}
-            height={2}
+            height={3}
             crawl={true}
-            showSpinner={true}
+            showSpinner={false}
             easing="ease"
             speed={200}
-            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            shadow=""
           />
           {children}
         </Providers>
