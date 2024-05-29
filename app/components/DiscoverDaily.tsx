ea88a6c17690f57e0a2b966aca12591a48f6ae40
dello <vicctheerapper@gmail.com>
2024-05-29 16:31:22 +0100

    djscbwdjv

--- app/components/DiscoverDaily.tsx
+++ app/components/DiscoverDaily.tsx
@@ -87,16 +87,14 @@ function DiscoverDaily() {
                 <div className="a z-40 w-[150px] sm:w-[130px] h-[45px] sm:h-[40px] shadow-md bg-gray-100 absolute top-0 right-0 rounded-bl-2xl rounded-tr-xl text-[18px] sm:text-[15px] font-normal text-slate-700 flex items-center justify-center">
                   Trending Today
                 </div>
-                <div className="inner w-full h-full relative overflow-hidden">
-                  <div className="mask rounded-xl w-full h-full absolute top-0 left-0 z-20 bg-gradient-to-tr from-[var(--background-color-1)] to-[#8c8b8b12]"></div>
-                  <Image
-                    unoptimized
-                    className="w-full h-full object-cover rounded-2xl"
-                    src={`https://themoviedb.org/t/p/w500_and_h282_face${result.backdrop_path}`}
-                    alt=""
-                    width={300}
-                    height={180}
-                  ></Image>
+                <div
+                  className="inner w-full h-full relative overflow-hidden rounded-xl"
+                  style={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/w500${result.backdrop_path})`,
+                    backgroundSize: "cover",
+                  }}
+                >
+                  <div className="mask w-full h-full absolute top-0 left-0 z-20 bg-gradient-to-tr from-[var(--background-color-1)] to-transparent"></div>
                   <div className="absolute bottom-4 left-4 z-30">
                     <p className="text-[14px] font-[500] word sm:text-[12px] text-[var(--color-3)]">
                       {result.media_type == "movie"
