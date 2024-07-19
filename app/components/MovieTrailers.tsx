82eb4ba148a376f4aa169c6ee1e1f9b1a0717715
Victor Faruna <vicctheerapper@gmail.com>
2024-07-19 13:03:47 +0100

    hlvhcvhsdjv

--- app/components/MovieTrailers.tsx
+++ app/components/MovieTrailers.tsx
@@ -29,13 +29,14 @@ export default function MovieTrailers({
         {trailers.results.slice(0, 1).map((trailer: any) => (
           <div
             key={trailer.id}
-            className="w-[400px] sm:w-[70vw] h-[240px] sm:h-[150px] rounded-xl sm:rounded-lg overflow-hidden relative"
+            className="w-[400px] sm:w-[50vw] h-[240px] sm:h-[100px] rounded-xl sm:rounded-lg overflow-hidden relative"
             style={{
               backgroundImage: `url(https://image.tmdb.org/t/p/w500${movieBanner})`,
+              backgroundSize: "cover",
               flex: "0 0 auto",
             }}
           >
-            <div className="mask absolute top-0 left-0 w-full h-[100%] bg-[#3a3a3a8d] flex items-center justify-center">
+            <div className="mask absolute top-0 left-0 w-full h-[100%] bg-[#0000008d] flex items-center justify-center">
               <div className="w-auo h-auto rounded-full bg-[#06040c] flex items-center justify-center cursor-pointer">
                 <i className="fa-solid fa-circle-play text-[#fff] text-[60px] sm:text-[45px]"></i>
               </div>
@@ -55,7 +56,7 @@ export default function MovieTrailers({
         >
           <div
             style={{ flex: "0 0 auto" }}
-            className="w-[400px] sm:w-[70vw] h-[240px] sm:h-[150px] rounded-xl sm:rounded-lg bg-gray-900 flex justify-center items-center cursor-pointer"
+            className="w-[400px] sm:w-[50vw] h-[240px] sm:h-[100px] rounded-xl sm:rounded-lg bg-gray-900 flex justify-center items-center cursor-pointer"
           >
             <p className="text-[13px] text-[lightgreen] cursor-pointer">
               Other Trailers on Youtube{" "}
