e5b8d32a2c847cabaa3e16653bfe5221fba47e9c
dello <vicctheerapper@gmail.com>
2024-07-23 18:39:30 +0100

    ds lllvhs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import Script from "next/script";
@@ -68,6 +69,7 @@ export default function RootLayout({
           shadow=""
         /> */}
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
