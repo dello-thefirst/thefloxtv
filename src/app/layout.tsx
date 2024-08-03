2b0e44bbaab57a3a1773d1bd5eeff6fb24894796
dello <vicctheerapper@gmail.com>
2024-08-03 21:31:01 +0100

    ef wvw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Script from "next/script";
 import { Kanit, Work_Sans } from "next/font/google";
 import ReactQueryProvider from "./providers/ReactQuery";
 import NextTopLoader from "nextjs-toploader";
+import { useEffect } from "react";
 
 const kanit = Kanit({
   weight: ["100", "200", "300", "400", "500", "600"],
@@ -37,6 +38,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',6134024,document.body||document.documentElement)`;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   return (
     <html lang="en">
       <head>
